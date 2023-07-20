cb2ac9e74c6c2f9866725eee71d23aed829f7fca
tsng <TrongSon.Nguyen@episerver.com>
2023-07-20 16:26:54 +0700

    Add antiforgery token to verify Save setting api
    Fixes: MAR-1630

--- src/EPiServer.Marketing.Testing.Web/ClientResources/Config/src/index.tsx
+++ src/EPiServer.Marketing.Testing.Web/ClientResources/Config/src/index.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
+
+const root = document.getElementById("root") as HTMLElement;
+const antiforgeryHeaderName: string = root.dataset.epiAntiforgeryHeaderName as string;
+const antiforgeryFormFieldName: string = root.dataset.epiAntiforgeryFormFieldName as string;
+const xsrfToken = document.getElementsByName(
+    antiforgeryFormFieldName
+)[0] as HTMLInputElement;
+const xsrfHeader = {
+    ...axios.defaults.headers,
+    [antiforgeryHeaderName]: xsrfToken.value,
+};
+axios.defaults.headers = xsrfHeader;
 
 ReactDOM.render(<App />, document.getElementById("root"));
